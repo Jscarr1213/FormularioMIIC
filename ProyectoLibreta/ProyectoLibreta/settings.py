1bd20caf3818bae5bc848b129bfdfc343818e563
Jscarr1213 <jesuscarreiroaguilar@gmail.com>
2025-06-05 22:59:47 -0600

    fifa

--- ProyectoLibreta/ProyectoLibreta/settings.py
+++ ProyectoLibreta/ProyectoLibreta/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w1478!#t(za^*pn%fll9aflzfe*zj)!06-!qyrbwru=qf4s26i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
